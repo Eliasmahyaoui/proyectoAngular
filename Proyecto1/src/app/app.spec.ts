ab2e692f6eedbb3263baccbbf9549a4e22a218a0
Elias Mahyaoui <eliasmahyaoui6@hotmail.com>
2025-12-16 18:43:44 +0100

    actualizado Bea

--- Proyecto1/src/app/app.spec.ts
+++ Proyecto1/src/app/app.spec.ts
@@ -18,6 +18,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, proyectoAngular');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Proyecto1');
   });
 });
--- Proyecto1/src/app/app.ts
+++ Proyecto1/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('proyectoAngular');
+  protected readonly title = signal('Proyecto1');
 }
