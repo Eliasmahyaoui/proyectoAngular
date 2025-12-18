fd88a4bcef380fee61c42a99b7b81478c248e770
Elias Mahyaoui <eliasmahyaoui6@hotmail.com>
2025-12-18 01:17:18 +0100

    Ejercicios Typescript hechos

--- Proyecto1/src/app/app.ts
+++ Proyecto1/src/app/app.ts
@@ -2,20 +2,53 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
+
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
+
 })
+
+
 export class App {
-protected readonly title = signal('Elias Fellah');
+  protected readonly title = signal('Elias Fellah');
 
   //Las declaraciones van fuera siempre,
-  nombre:String = 'Elias Mahyaoui';
-  edad:number= 27;
-  activo:boolean= true;
-  ngOnInit(){
-  //dentro de la funcion solo propiedades y metodos
+
+
+  // 1
+  nombre: String = 'Elias Mahyaoui';
+  edad: number = 27;
+  activo: boolean = true;
+
+
+  //2
+  calcularAreaRectangulo(base: number = 2, altura: number = 5) {
+    return base * altura;
+  }
+
+  //4
+
+  idType(id: number | string ) {
+
+    return id;
+  }
+
+  //5
+  arrayNumeros(numeros:[5,3,1,2]){
+    return numeros.sort();
+  }
+
+
+
+
+
+
+  ngOnInit() { //dentro de la funcion solo propiedades y metodos
+
+
+    //1
     this.nombre;
     this.edad;
     this.activo;
@@ -23,13 +56,43 @@ protected readonly title = signal('Elias Fellah');
     console.log(`Nombre:${this.edad}`);
     console.log(`Nombre:${this.activo}`);
 
+    //2
 
+    console.log('Resultado = ' + this.calcularAreaRectangulo(2, 5));
+
+
+    //3
+    interface Persona {   //¿Porque va aquí? dentro porque es cmoo un metodo una interface
+      nombre: string;
+      edad: number;
+      correo: string;
+    }
+
+    const usuario: Persona = {
+      nombre: 'pepe',
+      edad: 33,
+      correo: 'pepitoeldelospalotes.com'
+    }
+
+    console.log(usuario);
+
+
+    //4
+    const mensaje= this.idType(2);
+    console.log(mensaje);
+
+
+    //5
+    console.log(this.arrayNumeros([5,3,1,2]));
 
 
 
 
 
   }
+
+
+
 
 
 
