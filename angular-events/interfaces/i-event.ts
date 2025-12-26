877732fd7ca085e5a18e99ff4b832ff4e2b50bdd
Elias Mahyaoui <eliasmahyaoui6@hotmail.com>
2025-12-26 02:32:16 +0100

    actualizacion hasta array IEvent en proceso

---
+++ angular-events/interfaces/i-event.ts
@@ -0,0 +1,7 @@
+export interface IEvent {
+title:string;
+image:string;
+date:String;
+description:string;
+price:number;
+}
---
+++ angular-events/src/app/events-show/events-show.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-events-show',
+  imports: [],
+  templateUrl: './events-show.html',
+  styleUrl: './events-show.css',
+})
+export class EventsShow {
+
+  productos:IEvents[] = [
+    {
+
+    }
+  ];
+
+
+
+}
